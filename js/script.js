9250ae57635d1cdb74a059d08f6f8c754843501d
headmaster-hplztp <61901087+headmaster-hplztp@users.noreply.github.com>
2020-07-28 20:38:45 +0300

    Check script

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@
   const navigation = document.querySelector(".page-navigation");
   const links = Array.from(navigation.querySelectorAll("li a"));
   const path = window.location.pathname;
-  links.forEach(function(link) {
-    if (link.href.includes(path)) link.classList.add("active");
-  });
+  if (path == "/") {
+    navigation.querySelector("a[href='index.html']").classList.add("active")
+  } else {
+    links.forEach(function(link) {
+      if (link.href.includes(path)) link.classList.add("active");
+    });
+  }
 })()
